2644458bf3640fc88e509e44b067fbdaa03b0dcb
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2025-06-21 23:05:58 +0300

    add: types

--- 02_intro/01_intro_tls/Program.cs
+++ 02_intro/01_intro_tls/Program.cs
@@ -1,12 +1,4 @@
 ﻿
-
-
-
-
-
-
-
-
 //// comment
 ///*
 //    comment
@@ -71,25 +63,100 @@
 //Console.WriteLine("Vasia");
 
 
-int? a = null;
-Console.WriteLine(a);
+//int? a = null;
+//Console.WriteLine(a);
+
+#endregion
+
+
+#region Types
+// value types (structs)
+//bool a = true;
+
+//byte b = 3;         // 0 -> 255
+//sbyte c = -3;       // -128 -> 127
+
+//short d = -234;
+//ushort e = 23;
+
+//int f = -45;
+//uint g = 12;
+
+//long h = -123;
+//ulong i = 34;
+
+//float j = 4.56f;
+//double k = 4.67;
+//decimal l = 4.6m;
+
+//char m = '3';
+
+//// reference types (classes)
+
+//string n = "Vasia";
+//object o = null;
 
 
 
+//int a = 4;
+//int b = 4;
+//Console.WriteLine(a == b);
+
+//User c = new User();
+//User d = new User();
+//Console.WriteLine(c == d);
+
+
+//class User
+//{ }
+
+#endregion
+
+
+#region Console
+
+//Console.WriteLine("Vasia");
+//Console.Write("Petya\n");
+
+//string email = "vasia@mail.com";
+//int age = 34;
+//bool isAdmin = false;
+
+//Console.WriteLine(email);
+//Console.WriteLine(age);
+//Console.WriteLine(isAdmin);
+
+//Console.WriteLine($"email: {email}, age: {age}, isAdmin: {isAdmin}");
 
 
 
+//Console.Write("Enter your email: ");
+//string email = Console.ReadLine();
+//Console.WriteLine($"Hello {email}");
+
+//Console.Write("Enter your age: ");
+//int age = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"AGE: {age}");
 
 
 
+Console.SetCursorPosition(10, 3);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.DarkGreen;
 
+Console.WriteLine("VASIA");
 
+Console.ResetColor();
+Console.WriteLine("PETYA");
 
-
-
-
+int a = 4;
+int b = 6;
+Console.WriteLine($"Result = {a + b}");
 
 
 #endregion
+
+
+
 
 
---
+++ 02_intro/99_cpp_ex/99_cpp_ex.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+struct Point
+{
+    int x{};
+    int y{};
+};
+
+
+class User
+{
+public:
+    int id;
+    std::string email;
+};
+
+
+int main()
+{
+    /*Point a{ 3, 4 };
+    Point* b = new Point{ 3, 4 };
+
+    User c{ 101, "vasia@mail.com" };
+    User* d = new User{ 102, "petya@mail.com" };
+
+    bool e = true;*/
+
+
+    /*User* a = new User{};
+    User* b = new User{};
+    std::cout << a == b << '\n';*/
+
+    /*std::string input;
+    std::cin >> input;*/
+
+    int a = 4;
+    int b = 5;
+    std::cout << a + b;     // operator+(a, b)
+
+    
+    
+}
